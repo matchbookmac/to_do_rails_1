42e308c30bbebda2afc5e6cedb9bcb630e31942a
Ian MacDonald & Kevin Chen <student@epicodus.com>
2015-06-29 10:44:42 -0700

    Add task to list

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new(task_params)
+    if @task.save
+      flash[:notice] = "Task successfully added!"
+      redirect_to list_path(@task.list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629161033) do
+ActiveRecord::Schema.define(version: 20150629172041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
